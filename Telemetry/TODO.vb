d08a0f15da7d566761f4b64fc02cf8f48a861fca
NawakCreations <amaury.vanoorenberghe@laposte.net>
2017-12-04 21:41:01 +0100

    Update Telemetry/TODO.vb

--- Telemetry/TODO.vb
+++ Telemetry/TODO.vb
@@ -1,1 +1,3 @@
-'TODO : Modifier l'API pour la nouvelle interface
+' TODO : Modifier (Refaire) l'API pour la nouvelle interface
+' TODO : Ajouter la fen�tre GRAPH + enregistrement csv
+' TODO : Ajouter le wrapper pour le menu en bas de l'�cran (MESSAGES / CHRONOMETRE / ...) et ajouter le tout � PanelManager
