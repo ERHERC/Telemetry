604ac9cb82e932e2f76283fb28cd7d936cf7dd75
NawakCreations <amaury.vanoorenberghe@laposte.net>
2017-12-10 22:06:42 +0100

    Set Telemetry.Hardware.Data.Exchange splash screen

--- Telemetry.Hardware.Data.Exchange/My Project/Application.Designer.vb
+++ Telemetry.Hardware.Data.Exchange/My Project/Application.Designer.vb
@@ -34,5 +34,10 @@ Namespace My
         Protected Overrides Sub OnCreateMainForm()
             Me.MainForm = Global.Telemetry.Hardware.Data.Exchange.MainForm
         End Sub
+        
+        <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
+        Protected Overrides Sub OnCreateSplashScreen()
+            Me.SplashScreen = Global.Telemetry.Hardware.Data.Exchange.SplashForm
+        End Sub
     End Class
 End Namespace
