eb52e7b74581807c3685d38aecddaf3cb610e566
amaury <amaury@AMAURY-PC>
2018-03-05 22:47:24 +0100

    Deleted Telemetry.UnitTestsProject (since it's useless)

--- Telemetry/TODO.vb
+++ Telemetry/TODO.vb
@@ -1,11 +1,12 @@
-' TODO : [01] - Modifier (Refaire) l'API pour la nouvelle interface
+' TODO : [xx] - <<[01]>> - Modifier (Refaire) l'API pour la nouvelle interface
 ' TODO : [02] - Ajouter la fenêtre GRAPH + enregistrement csv (voir pour cls aussi)
 ' TODO : [03] - Changer le wrapper pour le menu en bas de l'écran (MESSAGES / CHRONOMETRE / ...) et ajouter le tout à PanelManager (Le transférer dans une fenêtre séparée elle-même référencée dans Globals)
 ' TODO : [04] - Ajouter le chronomètre
 ' TODO : [05] - Utiliser VB.Timer pour le chronomètre (Utiliser Mod , Math.Floor() et Math.Ceiling() pou les calculs)
-' TODO : [06] - Ajouter l'assistant de mise en route (fait à 50% [juste la GUI , pas les fonctionnalités])
+' TODO : [xx] - <<[06]>> - Ajouter l'assistant de mise en route (fait à 50% [juste la GUI , pas les fonctionnalités])
 ' TODO : [07] - Ajouter l'instantiation dynamique des types pour le lanceur de tests unitaires
 ' TODO : [08] - Détecter automatiquement les applications pour les ajouter au launcher (créer un type réutilisable pour le stockage d'informations dans Telemetry.Reusable)
 ' TODO : [09] - Créer les types de stockage dans Telemetry.Xml.Data
 ' TODO : [10] - Ajouter un message d'avertissement au launcher a propos de Windows Defender (API TCP) ou voir pour l'ajouter dans la classe de stockage (afficher le message au lancement de l'appli)
 ' TODO : [11] - Faire que le message de la tâche 10 apparaisse dans un panneau qui prends toute la fenêtre et qui à pour fond le contenu de la form mais flouté (je pense que ça peut être joli)
+' TODO : [12] - Ajouter un système de viewport pour Telemetry.Tests.Run
