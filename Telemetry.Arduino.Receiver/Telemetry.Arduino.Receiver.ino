a12a6290dd6e9489d1b238ca2a624bcc6ac49498
amaury <amaury@AMAURY-PC>
2018-03-09 23:00:40 +0100

    Added Arduino projects

---
+++ Telemetry.Arduino.Receiver/Telemetry.Arduino.Receiver.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Telemetry.ino
+ Created:	3/9/2018 11:00:25 PM
+ Author:	amaury
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
